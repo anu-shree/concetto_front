f4869c5e2690ffe87355ad34f2a79daa6e9bcc99
abhi0505 <abhishekranjan0505@gmail.com>
2019-09-28 20:37:51 +0530

    eventDetail

--- src/components/EventDetail/index.js
+++ src/components/EventDetail/index.js
@@ -1,7 +1,42 @@
 import React, { Component } from "react";
 import Loading from "../Loading";
+import About from "./About";
+import Rules from "./Rules";
+import Details from "./Details";
+import Register from "./Register";
 
 class EventDetail extends Component {
+  constructor(props){
+    super(props);
+      this.aboutShow=this.aboutShow.bind(this);
+      this.rulesShow = this.rulesShow.bind(this);
+      this.detailsShow = this.detailsShow.bind(this);
+      this.registerShow = this.registerShow.bind(this);
+  }
+state={
+  active:'about',
+};
+
+  aboutShow = (active) => {
+  this.setState({
+    active: 'about',
+  })
+}
+  rulesShow = (active) => {
+      this.setState({
+        active: 'rules',
+      })
+}
+  detailsShow = (active) => {
+      this.setState({
+        active: 'details',
+      })
+}
+  registerShow = (active) => {
+      this.setState({
+        active: 'register',
+      })
+}
   render() {
     const events =
       {
@@ -32,10 +67,10 @@ class EventDetail extends Component {
           />
           </div>
           <div className="btnFlex">
-            <button className="btn btn-2">HOME</button>
-            <button className="btn btn-2">RULES</button>
-            <button className="btn btn-2">DETAILS</button>
-            <button className="btn btn-2">ABOUT</button>
+            <button className={"btn btn-2 "+((this.state.active==='about')?'active-bottom':'')} onClick={this.aboutShow}>ABOUT</button>
+            <button className={"btn btn-2 "+((this.state.active==='rules')?'active-bottom':'')} onClick={this.rulesShow}>RULES</button>
+            <button className={"btn btn-2 "+((this.state.active==='details')?'active-bottom':'')} onClick={this.detailsShow}>DETAILS</button>
+            <button className={"btn btn-2 "+((this.state.active==='register')?'active-bottom':'')} onClick={this.registerShow}>REGISTER</button>
           </div>
         </div>
         <div>
@@ -69,6 +104,10 @@ class EventDetail extends Component {
           <div>
             {events.name}
           </div>
+          {this.state.active === 'about' && <About />}
+          {this.state.active === 'rules' && <Rules />}
+          {this.state.active === 'details' && <Details />}
+          {this.state.active==='register' && <Register/>}
         </div>
     </div>
     );
